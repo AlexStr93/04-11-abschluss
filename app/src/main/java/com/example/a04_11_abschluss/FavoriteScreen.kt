a7c1e0ee6ed54e9c0ffcc3734cdfdb58aa4b1b0b
Alexander Stracke <alex_stracke@gmx.de>
2025-03-25 12:28:22 +0100

    INIT:

--- app/src/main/java/com/example/a04_11_abschluss/FavoriteScreen.kt
+++ app/src/main/java/com/example/a04_11_abschluss/FavoriteScreen.kt
@@ -1,127 +1,210 @@
 package com.example.a04_11_abschluss
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.List
-import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.GridView
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.a04_11_abschluss.model.FavoriteCharacter
 import com.example.a04_11_abschluss.viewModel.FavoritesViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavoritesScreen(viewModel: FavoritesViewModel) {
     val favorites by viewModel.favoriteCharacters.collectAsState(initial = emptyList())
-
     var showDeleteDialog by remember { mutableStateOf(false) }
     var characterToDelete by remember { mutableStateOf<FavoriteCharacter?>(null) }
     var isGridView by remember { mutableStateOf(false) }
-
-    Scaffold(
-        floatingActionButton = {
-            FloatingActionButton(onClick = { isGridView = !isGridView }) {
-                Icon(
-                    imageVector = if (isGridView) Icons.AutoMirrored.Filled.List else Icons.Filled.GridView,
-                    contentDescription = "Ansicht wechseln"
-                )
-            }
-        }
-    ) { paddingValues ->
-        Column(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
-                .padding(16.dp)
-        ) {
-
-            Spacer(modifier = Modifier.height(16.dp))
-
-            if (favorites.isEmpty()) {
-                Text(text = "Keine Favoriten gespeichert", style = MaterialTheme.typography.bodyMedium)
-            } else {
-                if (isGridView) {
-                    LazyVerticalGrid(
-                        columns = GridCells.Fixed(2),
-                        modifier = Modifier.fillMaxSize(),
-                        contentPadding = PaddingValues(4.dp)
-                    ) {
-                        items(favorites.size) { index ->
-                            FavoriteCharacterCard(favorites[index], onDeleteClick = {
-                                characterToDelete = favorites[index]
-                                showDeleteDialog = true
-                            })
-                        }
-                    }
-                } else {
-                    LazyColumn(
-                        modifier = Modifier.fillMaxSize()
-                    ) {
-                        items(favorites) { character ->
-                            FavoriteCharacterCard(character, onDeleteClick = {
-                                characterToDelete = character
-                                showDeleteDialog = true
-                            })
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    // Bestätigungsdialog anzeigen, wenn showDeleteDialog == true
-    if (showDeleteDialog) {
-        AlertDialog(
-            onDismissRequest = { showDeleteDialog = false },
-            title = { Text("Favorit löschen") },
-            text = { Text("Sind Sie sicher, dass Sie diesen Favoriten entfernen möchten?") },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        characterToDelete?.let { viewModel.removeFavorite(it) }
-                        showDeleteDialog = false
-                    }
-                ) {
-                    Text("Ja")
-                }
-            },
-            dismissButton = {
-                TextButton(onClick = { showDeleteDialog = false }) {
-                    Text("Abbrechen")
-                }
-            }
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Hintergrundbild
+        Image(
+            painter = painterResource(id = R.drawable.onepiece_bg),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
         )
+        Scaffold(
+            containerColor = Color.Transparent,
+            floatingActionButton = {
+                FloatingActionButton(onClick = { isGridView = !isGridView }) {
+//                    Icon(
+//                        imageVector = if (isGridView) Icons.Default.GridView else Icons.Default.ViewList,
+//                        contentDescription = "Ansicht wechseln"
+//                    )
+                    Icon(
+                        painter = painterResource(
+                            id = if (isGridView) R.drawable.strohhut_list else R.drawable.strohhut_grid
+                        ),
+                        contentDescription = "Ansicht wechseln",
+                        modifier = Modifier.size(28.dp)
+                    )
+                }
+            }
+        ) { paddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
+                    .padding(16.dp)
+            ) {
+                Spacer(modifier = Modifier.height(16.dp))
+                if (favorites.isEmpty()) {
+                    Text(
+                        text = "Keine Favoriten gespeichert",
+                        style = MaterialTheme.typography.bodyLarge.copy(color = Color.White, fontSize = 20.sp)
+                    )
+                } else {
+                    if (isGridView) {
+                        LazyVerticalGrid(
+                            columns = GridCells.Fixed(2),
+                            modifier = Modifier.fillMaxSize(),
+                            contentPadding = PaddingValues(8.dp)
+                        ) {
+                            items(favorites.size) { index ->
+                                FavoriteCharacterCard(favorites[index], onDeleteClick = {
+                                    characterToDelete = favorites[index]
+                                    showDeleteDialog = true
+                                })
+                            }
+                        }
+                    } else {
+                        LazyColumn(
+                            modifier = Modifier.fillMaxSize()
+                        ) {
+                            items(favorites) { character ->
+                                FavoriteCharacterCard(character, onDeleteClick = {
+                                    characterToDelete = character
+                                    showDeleteDialog = true
+                                })
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (showDeleteDialog) {
+            AlertDialog(
+                onDismissRequest = { showDeleteDialog = false },
+                title = { Text("Favorit löschen") },
+                text = { Text("Willst du diesen Charakter aus deiner Crew entfernen?") },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            characterToDelete?.let { viewModel.removeFavorite(it) }
+                            showDeleteDialog = false
+                        }
+                    ) {
+                        Text("Aye!")
+                    }
+                },
+                dismissButton = {
+                    TextButton(onClick = { showDeleteDialog = false }) {
+                        Text("Abbrechen")
+                    }
+                }
+            )
+        }
     }
 }
-
 @Composable
 fun FavoriteCharacterCard(character: FavoriteCharacter, onDeleteClick: () -> Unit) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
             .padding(8.dp),
-        elevation = CardDefaults.cardElevation(4.dp)
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFFFF3C7)),
+        shape = RoundedCornerShape(12.dp),
+        elevation = CardDefaults.cardElevation(8.dp)
     ) {
         Row(
             modifier = Modifier
-                .fillMaxWidth()
                 .padding(16.dp),
-            horizontalArrangement = Arrangement.SpaceBetween
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
         ) {
             Column {
-                Text(text = character.name, style = MaterialTheme.typography.headlineSmall)
-                character.fruit?.let { Text(text = "Teufelsfrucht: $it", style = MaterialTheme.typography.bodyMedium) }
+                Text(
+                    text = " ${character.name}",
+                    style = MaterialTheme.typography.headlineSmall.copy(color = Color(0xFF6D4C41))
+                )
+                character.fruit?.let {
+                    Text(
+                        text = "Teufelsfrucht: $it",
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
             }
-
-            IconButton(onClick = { onDeleteClick() }) {
-                Icon(imageVector = Icons.Filled.Delete, contentDescription = "Löschen")
+            IconButton(onClick = onDeleteClick) {
+                Icon(
+                    painter = painterResource(id = R.drawable.teufelsfrucht_icon),
+                    contentDescription = "Löschen",
+                    modifier = Modifier.size(28.dp)
+                )
             }
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
